a6d619a97f06f26a10fed333fa72d48fce8c2241
ShukurluFakhri-12 <sukurovfexri21@gmail.com>
2026-02-01 10:50:05 +0400

    Update main.py
    Refactor: Added function and error handling

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import requests
 import pandas as pd
 import os
-url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
-response = requests.get(url).json()
-df = pd.DataFrame(response)
-increasing = df[(df['current_price'] > 500) & (df['price_change_percentage_24h'] > 0)]
-folder = 'Market_reports'
-if not os.path.exists(folder):
-    os.mkdir(folder)
-filepath = os.path.join(folder, 'topgainers.xlsx')
-increasing.to_excel(filepath, index=False)
-print('Process is completed! File is downloaded to folder')
+def Market_analysis()
+    try:
+        url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
+        response = requests.get(url).json()
+        df = pd.DataFrame(response)
+        increasing = df[(df['current_price'] > 500) & (df['price_change_percentage_24h'] > 0)]
+        folder = 'Market_reports'
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        filepath = os.path.join(folder, 'topgainers.xlsx')
+        increasing.to_excel(filepath, index=False)
+        print('Process is completed! File is downloaded to folder')
+    except Exception as e:
+        print(f"Error {e} occurred")
+Market_analysis()
