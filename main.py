eb34cc354e50b61ac0370eb0238afa612c027ac4
ShukurluFakhri-12 <sukurovfexri21@gmail.com>
2026-02-01 10:58:26 +0400

    Fix DataFrame creation and improve API error handling

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 import pandas as pd
 import os
-def Market_analysis()
+def market_analysis():
     try:
         url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
-        response = requests.get(url).json()
-        df = pd.DataFrame(response)
+        response = requests.get(url, timeout = 10)
+        data = response.json()
+        df = pd.DataFrame(data)
         increasing = df[(df['current_price'] > 500) & (df['price_change_percentage_24h'] > 0)]
         folder = 'Market_reports'
         if not os.path.exists(folder):
@@ -15,4 +16,4 @@ def Market_analysis()
         print('Process is completed! File is downloaded to folder')
     except Exception as e:
         print(f"Error {e} occurred")
-Market_analysis()
+market_analysis()
